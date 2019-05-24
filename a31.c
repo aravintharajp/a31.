2cf12efac6381fa0b5b714b46a90402cd0df7ac6
aravintharajp <50869424+aravintharajp@users.noreply.github.com>
2019-05-23 21:08:23 -0700

    Create a31.c

---
+++ a31.c
@@ -0,0 +1,18 @@
+int main()
+{
+    char s[100];
+int a=0,i=0;
+
+gets(s);
+while(s[i]!='\0')
+{
+    if(s[i]==' ')
+    
+        i++;
+        a++;
+        i++;
+    }
+    printf("%d ",a);
+}
+
+    
